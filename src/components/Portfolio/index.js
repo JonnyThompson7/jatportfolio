300c7306e96431b8728ac8b2c58972f85fc4a557
Jonathan Thompson <jathompson399@gmail.com>
2022-10-28 23:40:10 -0500

    finish portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,32 +18,32 @@ function Portfolio() {
       link: 'https://alexcourtney18.github.io/cocktail-doggo/'
     },
     {
-      picture: require('../../assets/runbuddy.JPG'),
-      name: 'Run Buddy',
-      description: 'This is a hypothetical website design for a company named Run Buddy. It was my first coding project',
-      repo: 'https://github.com/JonnyThompson7/run-buddy',
-      link: 'https://jonnythompson7.github.io/run-buddy/'
+      picture: require('../../assets/og.JPG'),
+      name: 'Jonathan Thompson Portfolio',
+      description: 'This was my first Portfolio, when you open the tab you will see the page title of "Portfolio" with 3 headers. Each header (About me, Work, and Contact Me) when clicked will take you to the section of the page containing information within that section. A little farther down on the page you will see my name, Jonathan Thompson, and my avatar who is bowing spanning the entire header.',
+      repo: 'https://github.com/JonnyThompson7/PortfolioJT',
+      link: 'https://jonnythompson7.github.io/PortfolioJT/'
     },
     {
-      picture: require('../../assets/runbuddy.JPG'),
-      name: 'Run Buddy',
-      description: 'This is a hypothetical website design for a company named Run Buddy. It was my first coding project',
-      repo: 'https://github.com/JonnyThompson7/run-buddy',
-      link: 'https://jonnythompson7.github.io/run-buddy/'
+      picture: require('../../assets/quiz.JPG'),
+      name: 'Quick Quiz',
+      description: 'In this project you will be taken to a site allowing you view scores or take a timed quiz with 5 questions all themed around JavaScript. If you choose to take the quiz the 90 second timer will begin and you have to answer the questions, but time is taken off for each wrong answer and if the timer hits 0, your run is over. Once all 5 questions have been answered, you are prompted with your score and the ability to add your name and score to scoreboard. After posting your score, you will be able to view all scores, clear all scores, or go back to the start where you can either view all scores again, or take the quiz!',
+      repo: 'https://github.com/JonnyThompson7/quickquiz',
+      link: 'https://jonnythompson7.github.io/quickquiz/'
     },
     {
-      picture: require('../../assets/runbuddy.JPG'),
-      name: 'Run Buddy',
-      description: 'This is a hypothetical website design for a company named Run Buddy. It was my first coding project',
-      repo: 'https://github.com/JonnyThompson7/run-buddy',
-      link: 'https://jonnythompson7.github.io/run-buddy/'
+      picture: require('../../assets/work.JPG'),
+      name: 'Work Day Scheduler',
+      description: 'Upon opening the page, you will be shown a color coded schedule for a 8 AM to 5 PM work day. There is a text box for every hour of the day where you can type the tasks you need to complete within the day. You will see a red textbox for the present hour, and everything in the future will be green and everything in the past will be in grey. Once the task is typed out make sure to click the save button since once it is clicked you will be able to leave or refresh the page, and your input will still be there!',
+      repo: 'https://github.com/JonnyThompson7/schedulemeplease',
+      link: 'https://jonnythompson7.github.io/schedulemeplease/'
     },
     {
-      picture: require('../../assets/runbuddy.JPG'),
-      name: 'Run Buddy',
-      description: 'This is a hypothetical website design for a company named Run Buddy. It was my first coding project',
-      repo: 'https://github.com/JonnyThompson7/run-buddy',
-      link: 'https://jonnythompson7.github.io/run-buddy/'
+      picture: require('../../assets/weather.JPG'),
+      name: 'Weather Dashboard',
+      description: 'Upon opening the page you will be brought to weather forecast search engine. You will be able to search a city, and have it weather display on the page. After completing your search, each city will get added to the search history. You will have a color prompting you about the UV index within your searched city, and you will also be able to see the 5 day forecast of the city with multiple weather measures.',
+      repo: 'https://github.com/JonnyThompson7/weatherdash',
+      link: 'https://jonnythompson7.github.io/weatherdash/'
     },
   ])
 
